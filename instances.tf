aa7656d78330ae1f4d8ae606a1ef86a38e8bd227
purushothamgoud <purushotham.314@gmail.com>
2022-08-16 15:48:55 +0530

    worker changes

--- instances.tf
+++ instances.tf
@@ -13,18 +13,19 @@ resource "aws_instance" "kubernetes_Server" {
   }
 
 }
-# 2 Instance Of Redhat Servers(t2.micro)
-resource "aws_instance" "redhat_servers" {
-  count                  = 2
-  ami                    = var.redhatami
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.demo_sg.id]
-  subnet_id              = element(aws_subnet.subnets.*.id, count.index)
-  key_name               = var.key_name
-  user_data              = file("init_script.sh")
-  tags = {
-    Name = "Redhat_Server_${count.index + 1}"
-  }
+# 2 Instance Of kubernetes worker Servers(t2.micro)
 
+resource "aws_instance" "kubernetes_workers" {
+  count                  = 1
+  ami                    = var.kubernetes_ami
+  instance_type          = var.worker_instance_type
+  vpc_security_group_ids = [aws_security_group.kubernetes_sg.id]
+  subnet_id              = element(aws_subnet.kubernetes_subnets.*.id, count.index)
+  key_name               = var.key_name
+
+  tags = {
+    Name = "kubernetes_servers"
+    Type = "kubernetes_worker"
+  }
 
 }
